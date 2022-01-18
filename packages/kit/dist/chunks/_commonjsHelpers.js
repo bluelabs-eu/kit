ab946857fcb52302cf73ed7d9dd566322befc95f
Ian Callaghan <33469816+ian-callaghan@users.noreply.github.com>
2022-01-18 14:07:14 +0000

    Add dist folders for installation

---
+++ packages/kit/dist/chunks/_commonjsHelpers.js
@@ -0,0 +1,22 @@
+var commonjsGlobal = typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
+
+function getAugmentedNamespace(n) {
+	if (n.__esModule) return n;
+	var a = Object.defineProperty({}, '__esModule', {value: true});
+	Object.keys(n).forEach(function (k) {
+		var d = Object.getOwnPropertyDescriptor(n, k);
+		Object.defineProperty(a, k, d.get ? d : {
+			enumerable: true,
+			get: function () {
+				return n[k];
+			}
+		});
+	});
+	return a;
+}
+
+function commonjsRequire (path) {
+	throw new Error('Could not dynamically require "' + path + '". Please configure the dynamicRequireTargets or/and ignoreDynamicRequires option of @rollup/plugin-commonjs appropriately for this require call to work.');
+}
+
+export { commonjsRequire as a, commonjsGlobal as c, getAugmentedNamespace as g };
